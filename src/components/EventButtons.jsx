920dc2535047a48a8ee1aafcf8a7b6e31d581598
Amiir25 <amirsadik636@gmail.com>
2025-07-02 18:14:23 +0300

    Add EventButtons.jsx for the scroll buttons

---
+++ src/components/EventButtons.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const EventButtons = () => {
+    return (
+        <>
+            <div className="flex items-center gap-6 pl-10">
+                <button>&lt;</button>
+                <span>2 / 5</span>
+                <button>&gt;</button>
+            </div>
+        </>
+    )
+}
+
+export default EventButtons;
