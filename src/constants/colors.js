d956a5608e83145be55b0a114aba3093e80a8541
Oumer Habib Badebo <oumerhb@gmail.com>
2025-08-11 14:47:14 +0300

    feat: add How we are built section

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,8 +1,13 @@
 // src/constants/colors.js
 const COLORS = {
   backgroundYellow: '#DFAD3B',   // bg-yellow-400
+  backgroundWhite: '#F8F5F2',     // bg-white
   textBlueDark: '#023665',       // text-blue-900
   textGrayDark: '#000000',       // text-gray-800
+  primary: '#1E3A8A', // Navy blue
+  accent: '#F59E0B',  // Amber/Gold
+  textPrimary: '#1F2937', // Gray-800
+  border: '#D1D5DB', // Gray-300
 };
 
 export default COLORS;
