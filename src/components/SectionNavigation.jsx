245987b82a440f84acc3a2106fc1b81339cdf344
Amiir25 <amirsadik636@gmail.com>
2025-10-05 10:10:47 +0300

    Add margin bottom on the navigation area in SectionNavigation.jsx

--- src/components/SectionNavigation.jsx
+++ src/components/SectionNavigation.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const SectionNavigation = ({ currentIndex, total, onPrev, onNext }) => {
 
     return (
-        <div className="flex items-center gap-6 w-[110px] h-[20px] ml-[137px] text-[#023665]">
+        <div className="flex items-center gap-6 w-[110px] h-[20px] ml-[137px] mb-10 text-[#023665]">
             {/* Left button */}
             <button
             onClick={onPrev}
