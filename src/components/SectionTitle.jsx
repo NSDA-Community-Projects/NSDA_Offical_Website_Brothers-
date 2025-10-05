1987326027349b27845a4264089d006d56389249
Amiir25 <amirsadik636@gmail.com>
2025-10-05 08:15:22 +0300

    Use a rectlangle instead of underline for the title in SectionTitle.jsx

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -2,17 +2,18 @@ import React from "react";
 
 const SectionTitle = () => {
     return (
-        <>
-            <div>
-                <h1 className="text-5xl font-bold">
-                    Recent<br />
-                    <span className="relative">
-                        Eve<span className="absolute left-1 -bottom-1 bg-[#023766] h-2 w-full"></span>
-                    </span>nts
-                </h1>
-                <p className="mt-6 md:mt-12 max-w-82">Amaizing moments await. Secure your spot and stay in the loop!</p>
-            </div>
-        </>
+        <div>
+            <h1 className="text-5xl font-bold pl-[82px] text-[#023665]">
+                Recent<br />
+                Events
+
+                <div className="w-[89px] h-[8px] bg-[#023665]"></div>
+            </h1>
+            
+            <p className="mt-6 md:mt-12 max-w-82">
+                Amaizing moments await. Secure your spot and stay in the loop!
+            </p>
+        </div>
     )
 }
 
