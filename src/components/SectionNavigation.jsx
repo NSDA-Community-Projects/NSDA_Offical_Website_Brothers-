b6b37f03539dbce49263269806dd509483277527
Amiir25 <amirsadik636@gmail.com>
2025-10-05 09:01:17 +0300

    Update the layout if the navigation area in SectionNavigation.jsx

--- src/components/SectionNavigation.jsx
+++ src/components/SectionNavigation.jsx
@@ -3,23 +3,23 @@ import React, { useState } from "react";
 const SectionNavigation = ({ currentIndex, total, onPrev, onNext }) => {
 
     return (
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-6 w-[110px] h-[20px] ml-[137px] text-[#023665]">
             {/* Left button */}
             <button
             onClick={onPrev}
-            className="px-3 py-2 bg-gray-200 rounded-full hover:bg-gray-300">
+            className="">
                 &lt;
             </button>
 
             {/* Slide counter */}
             <span className="font-semibold">
-                {currentIndex}/{total}
+                {currentIndex} / {total}
             </span>
             
             {/* Right button */}
             <button
             onClick={onNext}
-            className="px-3 py-2 bg-gray-200 rounded-full hover:bg-gray-300">
+            className="">
                 &gt;
             </button>
         </div>
