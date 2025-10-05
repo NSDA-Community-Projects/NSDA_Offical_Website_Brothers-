c2521b7f1c9654b141bcf589332b18fe12622cc6
Amiir25 <amirsadik636@gmail.com>
2025-10-05 10:19:15 +0300

    Update the color and size of the text on the sliding image in ImageCarousel.jsx

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -24,9 +24,9 @@ const ImageCarousel = ({ eventImages, onSlideChange, navigationRef }) => {
                 <SwiperSlide key={i}>
                     <div className="h-full">
                         <img src={image.image} alt="Carousel Image" className="w-[360px] h-[420px] object-cover" />
-                        <div className="p-3 bg-white">
-                            <h3 className="font-semibold">Watch Now</h3>
-                            <p className="text-sm text-gray-500">{image.description}</p>
+                        <div className="text-sm  py-3">
+                            <h3 className="text-gray-500">Watch Now</h3>
+                            <p className="font-semibold mt-2">{image.description}</p>
                         </div>
                     </div>
                 </SwiperSlide>
